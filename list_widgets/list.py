60ccc33fbc78a3e16e5ed2ce3a5e667c358e02d1
Taro Yamashita <taro@panther-king.com>
2012-03-07 08:13:33 +0900

    Complete "Qyolk I" tutorial

--- list_widgets/list.py
+++ list_widgets/list.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'list.ui'
 #
-# Created: Fri Mar 02 12:45:24 2012
+# Created: Sat Mar 03 09:09:07 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -20,9 +20,27 @@ class Ui_QYolk(object):
         QYolk.resize(800, 600)
         self.centralwidget = QtGui.QWidget(QYolk)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.treeWidget = QtGui.QTreeWidget(self.centralwidget)
-        self.treeWidget.setGeometry(QtCore.QRect(10, 10, 781, 561))
-        self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
+        self.pkgTabs = QtGui.QTabWidget(self.centralwidget)
+        self.pkgTabs.setGeometry(QtCore.QRect(10, 10, 781, 551))
+        self.pkgTabs.setObjectName(_fromUtf8("pkgTabs"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.allList = QtGui.QTreeWidget(self.tab)
+        self.allList.setGeometry(QtCore.QRect(10, 10, 751, 501))
+        self.allList.setObjectName(_fromUtf8("allList"))
+        self.pkgTabs.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.activeList = QtGui.QTreeWidget(self.tab_2)
+        self.activeList.setGeometry(QtCore.QRect(10, 10, 751, 501))
+        self.activeList.setObjectName(_fromUtf8("activeList"))
+        self.pkgTabs.addTab(self.tab_2, _fromUtf8(""))
+        self.tab_3 = QtGui.QWidget()
+        self.tab_3.setObjectName(_fromUtf8("tab_3"))
+        self.notActiveList = QtGui.QTreeWidget(self.tab_3)
+        self.notActiveList.setGeometry(QtCore.QRect(10, 10, 751, 501))
+        self.notActiveList.setObjectName(_fromUtf8("notActiveList"))
+        self.pkgTabs.addTab(self.tab_3, _fromUtf8(""))
         QYolk.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(QYolk)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 19))
@@ -33,11 +51,21 @@ class Ui_QYolk(object):
         QYolk.setStatusBar(self.statusbar)
 
         self.retranslateUi(QYolk)
+        self.pkgTabs.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(QYolk)
 
     def retranslateUi(self, QYolk):
         QYolk.setWindowTitle(QtGui.QApplication.translate("QYolk", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.treeWidget.headerItem().setText(0, QtGui.QApplication.translate("QYolk", "Col0", None, QtGui.QApplication.UnicodeUTF8))
-        self.treeWidget.headerItem().setText(1, QtGui.QApplication.translate("QYolk", "Col1", None, QtGui.QApplication.UnicodeUTF8))
-        self.treeWidget.headerItem().setText(2, QtGui.QApplication.translate("QYolk", "Col2", None, QtGui.QApplication.UnicodeUTF8))
+        self.allList.headerItem().setText(0, QtGui.QApplication.translate("QYolk", "Col0", None, QtGui.QApplication.UnicodeUTF8))
+        self.allList.headerItem().setText(1, QtGui.QApplication.translate("QYolk", "Col1", None, QtGui.QApplication.UnicodeUTF8))
+        self.allList.headerItem().setText(2, QtGui.QApplication.translate("QYolk", "Col2", None, QtGui.QApplication.UnicodeUTF8))
+        self.pkgTabs.setTabText(self.pkgTabs.indexOf(self.tab), QtGui.QApplication.translate("QYolk", "All Packages", None, QtGui.QApplication.UnicodeUTF8))
+        self.activeList.headerItem().setText(0, QtGui.QApplication.translate("QYolk", "Col0", None, QtGui.QApplication.UnicodeUTF8))
+        self.activeList.headerItem().setText(1, QtGui.QApplication.translate("QYolk", "Col1", None, QtGui.QApplication.UnicodeUTF8))
+        self.activeList.headerItem().setText(2, QtGui.QApplication.translate("QYolk", "Col2", None, QtGui.QApplication.UnicodeUTF8))
+        self.pkgTabs.setTabText(self.pkgTabs.indexOf(self.tab_2), QtGui.QApplication.translate("QYolk", "Active", None, QtGui.QApplication.UnicodeUTF8))
+        self.notActiveList.headerItem().setText(0, QtGui.QApplication.translate("QYolk", "Col0", None, QtGui.QApplication.UnicodeUTF8))
+        self.notActiveList.headerItem().setText(1, QtGui.QApplication.translate("QYolk", "Col1", None, QtGui.QApplication.UnicodeUTF8))
+        self.notActiveList.headerItem().setText(2, QtGui.QApplication.translate("QYolk", "Col2", None, QtGui.QApplication.UnicodeUTF8))
+        self.pkgTabs.setTabText(self.pkgTabs.indexOf(self.tab_3), QtGui.QApplication.translate("QYolk", "Not Active", None, QtGui.QApplication.UnicodeUTF8))
 
--- list_widgets/start.py
+++ list_widgets/start.py
@@ -14,12 +14,13 @@ class StartQt4(QtGui.QMainWindow):
         self.ui = Ui_QYolk()
         self.ui.setupUi(self)
 
-        self.ui.treeWidget.setColumnWidth(0, 200)
-        self.ui.treeWidget.setColumnWidth(1, 100)
+        # All packages
+        self.ui.allList.setColumnWidth(0, 200)
+        self.ui.allList.setColumnWidth(1, 200)
 
         packages = yolklib.Distributions()
         for pkg in packages.get_distributions("all"):
-            a = QtGui.QTreeWidgetItem(self.ui.treeWidget)
+            a = QtGui.QTreeWidgetItem(self.ui.allList)
             pk = str(pkg[0]).split(" ")
             if pkg[1]:
                 status = "Active"
@@ -31,6 +32,39 @@ class StartQt4(QtGui.QMainWindow):
             a.setText(0, pk[0])
             a.setText(1, pk[1])
             a.setText(2, status)
+
+        # Active packages
+        self.ui.activeList.setColumnWidth(0, 200)
+        self.ui.activeList.setColumnWidth(1, 200)
+
+        for pkg in packages.get_distributions("active"):
+            a = QtGui.QTreeWidgetItem(self.ui.activeList)
+            pk = str(pkg[0]).split(" ")
+            a.setText(0, pk[0])
+            a.setText(1, pk[1])
+            a.setText(2, "Active")
+
+        # Not active packages
+        self.ui.notActiveList.setColumnWidth(0, 200)
+        self.ui.notActiveList.setColumnWidth(1, 200)
+
+        for pkg in packages.get_distributions("nonactive"):
+            a = QtGui.QTreeWidgetItem(self.ui.notActiveList)
+            pk = str(pkg[0]).split(" ")
+            a.setText(0, pk[0])
+            a.setText(1, pk[1])
+            a.setText(2, "Not Active")
+
+        # Signals
+        QtCore.QObject.connect(self.ui.pkgTabs, QtCore.SIGNAL("currentChanged(int)"), self.tab_change)
+
+    def tab_change(self, tab_id):
+        if tab_id == 0:
+            self.ui.infoLabel.setText("<b>QYolk</b>: Browsing all installed cheeseshop packages")
+        elif tab_id == 1:
+            self.ui.infoLabel.setText("<b>QYolk</b>: Browsing active packages")
+        elif tab_id == 2:
+            self.ui.infoLabel.setText("<b>QYolk</b>: Browsing not active packages (older versions)")
 
 
 if __name__ == "__main__":
