f347f427939abd5fb7d39f04a689b9dba588a9b8
Taro Yamashita <taro@panther-king.com>
2012-03-19 08:39:59 +0900

    Complete "First programs" tutorial

---
+++ first_porgrams/message_box.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("Message box")
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, "Message",
+                                           u"本当に終了する？", QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
---
+++ first_porgrams/tooltip.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont("SanSerif", 10))
+        self.setToolTip("This is a <b>QWidget</b> widget")
+
+        btn = QtGui.QPushButton("Button", self)
+        btn.setToolTip("This is a <b>QPushButton</b> widget")
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("Tooltips")
+        self.show()
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
