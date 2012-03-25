5ed3750edbe762f0e32f42e6652659fab4c92833
Taro Yamashita <taro@panther-king.com>
2012-03-26 08:31:35 +0900

    Complete "Dialogs" tutorial

---
+++ dialogs/filedialog.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(self.textEdit)
+        self.statusBar()
+
+        openFile = QtGui.QAction(QtGui.QIcon("open.png"), "Open", self)
+        openFile.setShortcut("Ctrl+O")
+        openFile.setStatusTip("Open new File")
+        openFile.triggered.connect(self.showDialog)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu("&File")
+        fileMenu.addAction(openFile)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle("File dialog")
+        self.show()
+
+    def showDialog(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, "Open file", "/home")
+        f = open(fname, "r")
+        with f:
+            data = f.read()
+            self.textEdit.setText(data)
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ dialogs/fontdialog.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        vbox = QtGui.QVBoxLayout()
+
+        btn = QtGui.QPushButton("Dialog", self)
+        btn.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        btn.move(20, 20)
+        vbox.addWidget(btn)
+        btn.clicked.connect(self.showDialog)
+
+        self.lbl = QtGui.QLabel("Knowledge only matters", self)
+        self.lbl.move(130, 20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle("Font dialog")
+        self.show()
+
+    def showDialog(self):
+        font, ok = QtGui.QFontDialog.getFont()
+        if ok:
+            self.lbl.setFont(font)
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
